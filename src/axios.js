85222859f9fcc46eb78cd04b0359bb40f745fd86
Matthew Lee <ml8423799@gmail.com>
2024-05-22 18:05:49 +0700

    updated

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "https://jade-uninterested-dog.cyclic.app",
+	// baseURL: "https://jade-uninterested-dog.cyclic.app",
+	baseURL: "https://todo-ser-ver-jzmm.vercel.app/",
 });
 export default instance
