b6a747791a862830228f1dcd2ac2b23d2e2243de
Matthew Lee <ml8423799@gmail.com>
2024-06-09 10:04:16 +0700

    add another BaseURL to testing

--- src/axios.js
+++ src/axios.js
@@ -3,5 +3,6 @@ import axios from "axios";
 const instance = axios.create({
 	// baseURL: "https://jade-uninterested-dog.cyclic.app",
 	baseURL: "https://todo-ser-ver-jzmm.vercel.app/",
+	//baseURL: "http://localhost:7000",
 });
 export default instance
