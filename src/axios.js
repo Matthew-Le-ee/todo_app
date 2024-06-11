c15156d80deddb00cb4ff6263d3d740dffef61a3
Matthew Lee <ml8423799@gmail.com>
2024-06-11 14:56:17 +0700

    change dev url

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
 	// baseURL: "https://jade-uninterested-dog.cyclic.app",
-	baseURL: "https://todo-ser-ver-jzmm.vercel.app/",
+	baseURL: "https://todo-ser-ver.vercel.app/",
 	//baseURL: "http://localhost:7000",
 });
 export default instance
